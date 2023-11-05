0609caf60a28321fa41351e065f6c3e32ae45736
Barbra <barbra.kachembere@gmail.com>
2023-11-04 20:03:28 -0700

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+	if not sentence:
+		sentence = None
+	if sentence:
+		else:
+		sen_len = 0
+	return (sen_len, sentence if not sentence else sentence[:1])
